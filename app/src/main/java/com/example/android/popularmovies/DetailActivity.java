acd0224e9c9f7247f24b3074b5e37ad6e325f399
Michael Hsieh <mdhsieh8@gmail.com>
2020-04-03 22:15:00 -0700

    feat: Add a third video button.

--- app/src/main/java/com/example/android/popularmovies/DetailActivity.java
+++ app/src/main/java/com/example/android/popularmovies/DetailActivity.java
@@ -6,10 +6,12 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -41,6 +43,10 @@ public class DetailActivity extends AppCompatActivity {
     private TextView userRatingDisplay;
     private TextView releaseDateDisplay;
 
+    private Button firstVideoButton;
+    private Button secondVideoButton;
+    private Button thirdVideoButton;
+
     private Movie movie;
 
     // Movie key when retrieving Intent
@@ -54,7 +60,13 @@ public class DetailActivity extends AppCompatActivity {
     private String releaseDate;
 
     // ArrayList to hold all the YouTube URLs of this Movie
+    // We will only display maximum 3 videos
     private static List<String> videoUrls = new ArrayList<>();
+
+    // the position of the videos in the ArrayList
+    private int FIRST_VIDEO_INDEX = 0;
+    private int SECOND_VIDEO_INDEX = 1;
+    private int THIRD_VIDEO_INDEX = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,6 +83,10 @@ public class DetailActivity extends AppCompatActivity {
         synopsisDisplay = findViewById(R.id.tv_display_synopsis);
         userRatingDisplay = findViewById(R.id.tv_display_user_rating);
         releaseDateDisplay = findViewById(R.id.tv_display_release_date);
+
+        firstVideoButton = findViewById(R.id.btn_video_1);
+        secondVideoButton = findViewById(R.id.btn_video_2);
+        thirdVideoButton = findViewById(R.id.btn_video_3);
 
         // get the Movie from Intent
         Intent intentThatStartedThisActivity = getIntent();
@@ -116,11 +132,6 @@ public class DetailActivity extends AppCompatActivity {
                     }
                 });
 
-                /*
-                    Get the video URLs from this movie in a background task
-                 */
-                new FetchVideosFromMovieTask().execute(id);
-
                 // click on favorites button
                 favoritesImageView.setOnClickListener(new View.OnClickListener() {
                     @Override
@@ -154,6 +165,57 @@ public class DetailActivity extends AppCompatActivity {
             {
                 Log.e(TAG, "Movie is null");
             }
+
+            /*
+                Get the video URLs from this movie in a background task
+             */
+            new FetchVideosFromMovieTask().execute(id);
+
+            /*
+                Click on video button 1. Play the corresponding video if it exists.
+             */
+            firstVideoButton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    if (videoUrls != null && videoUrls.size() >= 1) {
+                        String videoUrl = videoUrls.get(FIRST_VIDEO_INDEX);
+                        playVideo(videoUrl);
+                    }
+                }
+            });
+
+            /*
+                Click on video button 2. Play the corresponding video if it exists.
+             */
+            secondVideoButton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    if (videoUrls != null && videoUrls.size() >= 2) {
+                        String videoUrl = videoUrls.get(SECOND_VIDEO_INDEX);
+                        playVideo(videoUrl);
+                    }
+                }
+            });
+
+            /*
+                Click on video button 3. Play the corresponding video if it exists.
+             */
+            thirdVideoButton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    if (videoUrls != null && videoUrls.size() >= 3) {
+                        String videoUrl = videoUrls.get(THIRD_VIDEO_INDEX);
+                        playVideo(videoUrl);
+                    }
+                }
+            });
+        }
+    }
+
+    private void playVideo(String urlText) {
+        Uri videoUri = Uri.parse(urlText);
+        Intent playVideoIntent = new Intent(Intent.ACTION_VIEW);
+        playVideoIntent.setData(videoUri);
+        if (playVideoIntent.resolveActivity(getPackageManager()) != null) {
+            Log.d(TAG, "starting video using URL " + urlText);
+            startActivity(playVideoIntent);
         }
     }
 
